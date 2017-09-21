a897ee2f036b735adb6ca1772a2750a10a846d2a
ProjetodeComputero <guigui.10@gmail.com>
2017-09-21 19:04:35 -0300

    string

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <sys/types.h>
 #include <regex.h>
 #include <stdio.h>
- 
+
 #define MAX_MATCHES 1 //The maximum number of matches allowed in a single string
- 
+
 void match(regex_t *pexp, char *sz) {
 	regmatch_t matches[MAX_MATCHES]; //A list of the matches in the string (a list of 1)
 	//Compare the string to the expression
@@ -14,28 +14,22 @@ void match(regex_t *pexp, char *sz) {
 		printf("\"%s\" does not match\n", sz);
 	}
 }
- 
+
 int main() {
 	int rv;
 	regex_t exp; //Our compiled expression
 	//1. Compile our expression.
 	//Our regex is "-?[0-9]+(\\.[0-9]+)?". I will explain this later.
 	//REG_EXTENDED is so that we can use Extended regular expressions
-	rv = regcomp(&exp, "-?[0-9]+(\\.[0-9]+)?", REG_EXTENDED);
+  //STRING AEEEEEOOOOOO
+	rv = regcomp(&exp, "\"[^\\|\\?\b\f\n\r\t\\]*\"", REG_EXTENDED);
 	if (rv != 0) {
 		printf("regcomp failed with %d\n", rv);
 	}
 	//2. Now run some tests on it
-	match(&exp, "0");
-	match(&exp, "0.");
-	match(&exp, "0.0");
-	match(&exp, "10.1");
-	match(&exp, "-10.1");
-	match(&exp, "a");
-	match(&exp, "a.1");
-	match(&exp, "0.a");
-	match(&exp, "0.1a");
-	match(&exp, "hello");
+	match(&exp, "\"asd\"");
+	match(&exp, "\"cccc\bcc\"");
+	match(&exp, "\"hel\nlo\"");
 	//3. Free it
 	regfree(&exp);
 	return 0;
